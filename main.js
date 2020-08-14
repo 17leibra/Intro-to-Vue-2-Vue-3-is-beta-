64c090d6dfd5f035fdd8b64b68cbecfa5a4dd27f
Brayden_Leicester <55036213+17leibra@users.noreply.github.com>
2020-08-14 17:18:55 +1200

    Done lesson 2 and 3

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
        product: 'Socks',
-       description: 'These are cool socks' 
+       description: 'These are cool socks',
+       image: './assets/images/socks_green.jpg',
+       link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
+       inventory: 100,
+       onSale: true,
     }
 }) 
