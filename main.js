22199be5eabf8c71571639e7ed281095340b8b47
Brayden_Leicester <55036213+17leibra@users.noreply.github.com>
2020-08-16 11:28:43 +1200

    done lesson 7

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
 var app = new Vue({
     el: '#app',
     data: {
+       brand: 'Vue Mastery',
        product: 'Socks',
        description: 'These are cool socks',
-       image: './assets/images/socks_green.jpg',
-       link: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
-       inventory: 100,
+       selectedVariant: 0,
+       link: 'https://www.vuemastery.com/',
+       inventory: 10,
        onSale: true,
        details: ["80% cotton","20% polyester","Gender-neutral"],
        variants: [
            {
             variantId: 2234,
-            variantColor: "green"
+            variantColor: "green",
+            variantImage: './assets/images/socks_green.jpg',
+            variantQuantity: 10
            },
            {
             variantId: 2235,
-            variantColor: "blue"
+            variantColor: "blue",
+            variantImage: './assets/images/socks_blue.jpg',
+            variantQuantity: 0
            }
        ],
        sizes: ['S','M','L','XL'],
+       cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart ++
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        },
+        removeFromCart(){
+            this.cart --
+        }
+
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        saleType(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product+ ' are on sale'
+            }
+        }
     }
 }) 
